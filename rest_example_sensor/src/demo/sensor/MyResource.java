83aceeff391d0540ba26b7d440414b46e0befcfc
Paola Carreno <paoandrecm@Paolas-MBP-2.fritz.box>
2023-06-22 13:43:27 +0200

    Task 2

--- rest_example_sensor/src/demo/sensor/MyResource.java
+++ rest_example_sensor/src/demo/sensor/MyResource.java
@@ -5,11 +5,34 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import java.time.Instant;
+
 @Path("myresource")
 public class MyResource {
+	private SensorValue sensorValue = new SensorValue();
+
 	@GET
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public SensorValue getSensorValue() {
-		return new SensorValue();
+		sensorValue.updateValue();
+		return sensorValue;
 	}
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getSensorValueAsCSV() {
+		// Retrieve the time value and format it
+		Instant time = sensorValue.getTime();
+		String formattedTime = time.toString();
+
+		// Retrieve the value and format it
+		double value = sensorValue.getValue();
+		String formattedValue = Double.toString(value);
+
+		// Format the data as CSV: <time>,<value>
+		String csv = formattedTime + "," + formattedValue;
+
+		return csv;
+	}
+
 }
--- rest_example_sensor/src/demo/sensor/SensorValue.java
+++ rest_example_sensor/src/demo/sensor/SensorValue.java
@@ -1,21 +1,42 @@
 package demo.sensor;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.time.Instant;
+import java.util.Random;
 
-@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "battery")
+
 public class SensorValue {
-	@XmlElement
+	@XmlElement (name = "capacity")
+	public String capacity = "43.2 Wh";
+
 	@XmlJavaTypeAdapter(MyInstantAdapter.class)
 	public Instant time;
 	public double value;
 
 	public SensorValue() {
 		time = Instant.now();
-		value = Math.random() * 100;
+		value = 100;
 	}
+
+	public Instant getTime(){
+		return time;
+	}
+
+		public double getValue(){
+		return value;
+	}
+
+	public void updateValue() {
+        Random random = new Random();
+        int randomValue = random.nextInt(5) + 1; // Generates a random value between 1 and 5
+        value -= randomValue;
+    }
 
 	@Override
 	public String toString() {
